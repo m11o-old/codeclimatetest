d63f296e872d755b577d819fc89aafc89af2c035
m11o <velonica2227@icloud.com>
2020-04-19 10:49:32 +0900

    feat: update

--- test/codeclimatetest_test.rb
+++ test/codeclimatetest_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class CodeclimatetestTest < Minitest::Test
+class CodeclimatetestTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::Codeclimatetest::VERSION
   end
