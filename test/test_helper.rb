ebcb95a17415632b6b348001e25711bad509a00a
m11o <velonica2227@icloud.com>
2020-04-19 14:19:15 +0900

    feat: update

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,11 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require 'codeclimatetest'
 
 require 'simplecov'
-require 'test/unit'
-
 SimpleCov.start
 
 if ENV['CODECOV_TOKEN']
   require 'codecov'
-  SimpleCov.formatter =  SimpleCov::Formatter::Codecov
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
+
+require 'test/unit'
